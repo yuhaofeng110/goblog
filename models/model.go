879fe2a0346f0c155156c1ab96413b405e178636
deepzz0 <deepzz.qi@gmail.com>
2016-07-01 22:26:21 +0800

    瘦身

--- models/model.go
+++ models/model.go
@@ -14,12 +14,17 @@ import (
 )
 
 const (
-	DB         = "newblog" // database数据库
-	C_USER     = "user"    // collections表
-	C_TOPIC    = "topic"
-	C_TOPIC_ID = "topic_id" // 文章ID计数
-	C_REQUEST  = "request"  // 浏览记录
-	C_CONFIG   = "config"   // 配置
+	// database name
+	DB = "newblog"
+	// user table
+	C_USER  = "user"
+	C_TOPIC = "topic"
+	// id
+	C_TOPIC_ID = "topic_id"
+	// request
+	C_REQUEST = "request"
+	// config
+	C_CONFIG = "config"
 )
 
 const (
@@ -38,10 +43,11 @@ func init() {
 	if err := ip17mon.Init(path + "/conf/17monipdb.dat"); err != nil {
 		log.Fatal(err)
 	}
-	// 以下三句保证调用顺序
+	//
 	UMgr.loadUsers()
 	Blogger = UMgr.Get("deepzz")
-	if Blogger == nil { // 从配置初始化用户
+	// not found account
+	if Blogger == nil {
 		initAccount()
 	}
 	TMgr.loadTopics()
@@ -75,7 +81,6 @@ func initAccount() {
 	Blogger = UMgr.Get("deepzz")
 }
 
-// 新的一天
 func timer() {
 	t := time.NewTicker(time.Minute)
 	Today := time.Now()
